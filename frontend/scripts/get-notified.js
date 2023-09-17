8175d5989c9ea4724218493dd0bb992b0d48d34f
tpauldike <topman4loveworld@gmail.com>
2023-09-17 17:04:46 +0000

    introduce a new script

---
+++ frontend/scripts/get-notified.js
@@ -0,0 +1,10 @@
+const getNotifiedBtn = document.getElementById('get-notified-btn');
+const acceptBtn = document.getElementById('accept');
+const declineBtn = document.getElementById('decline');
+const askForWhatsappContact = document.getElementById('get-notified-container');
+const getWhatsappNumber = document.getElementById('number-input');
+const submitWhastappNumber = document.getElementById('submit-whatsapp-no');
+
+getNotifiedBtn.addEventListener('click', () => {
+    askForWhatsappContact.style.display = 'block';
+});
