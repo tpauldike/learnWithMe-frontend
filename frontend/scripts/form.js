c0e647adc5fda985593c3ce113e27ceec165be0f
tpauldike <topman4loveworld@gmail.com>
2023-09-19 10:47:12 +0100

    added the missing meta tag

--- frontend/scripts/form.js
+++ frontend/scripts/form.js
@@ -1,12 +1,9 @@
-// const apiUrl = 'phone-number-validation-api.p.rapidapi.com/checkNumber';
-// const apiKey = 'c9728f9495msh27f9bfdf197d54bp1afce1jsned9501dbdc44';
 const aboutNavBtn = document.getElementById('about');
 const contactTopman = document.getElementById('contact_topman');
 const subscribeNavBtn = document.getElementById('subscribe');
 const comingSoon = document.getElementById('coming-soon');
 const form = document.querySelector("form");
 const submitForm = document.querySelector("#submit-form");
-
 
 
 const displayComingSoon = () => {
@@ -35,5 +32,3 @@ document.getElementById('schedule').addEventListener('input', () => {
 //   const data = Object.fromEntries(formData.entries());
 //   console.log(data);
 // });
-
-// document.addEventListener('DOMContentLoaded', ...
